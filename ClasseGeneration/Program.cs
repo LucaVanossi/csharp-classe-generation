96923f8e1df2a242e05e7ed24b7bdc886e1f37af
LucaVanossi <vanossi.luca13@gmail.com>
2022-04-06 18:36:54 +0200

    Esercizio ok

--- ClasseGeneration/Program.cs
+++ ClasseGeneration/Program.cs
@@ -10,6 +10,10 @@ string sEta = "";
 
 int iEta = 0;
 
+string[] CopiaNomi = { };
+string[] CopiaCognomi = {};
+int[] CopiaEta = {};
+
 do  
 {
     sNumeroAlunni = Console.ReadLine();
@@ -23,19 +27,81 @@ int[] Eta = new int[NumeroAlunni];
 
 for (int i = 0; i < NumeroAlunni; i++)
 {
-    Console.WriteLine("Nome: " + (i+1));
-    Nomi[i] = Console.ReadLine();
-    Console.WriteLine("Cognome: " + (i+1));
-    Cognomi[i] = Console.ReadLine();
-    Console.WriteLine("Età: " + (i+1));
-    do
-    {
-        sEta = Console.ReadLine();
-    } while (!int.TryParse(sEta, out iEta) || iEta > 40);
-    Eta[i] = iEta;
+    InserisciAlunni(i);
 }
 
 StampaArrayAlunni(Cognomi, Nomi, Eta);
+
+if (NumeroAlunni < 10)
+{
+    Console.WriteLine("Vuoi inserire un nuovo alunno? (si/no)");
+    string RispostaUtente = Console.ReadLine();
+    if (RispostaUtente=="si") 
+    {
+        CopiaNomi = new string[NumeroAlunni+1];
+        CopiaCognomi = new string[NumeroAlunni + 1];
+        CopiaEta = new int[NumeroAlunni + 1];
+        for (int i = 0; i < Nomi.Length; i++)
+        {
+            CopiaNomi[i] = Nomi[i];
+            CopiaCognomi[i] = Cognomi[i];  
+            CopiaEta[i] = Eta[i];
+        }
+        Console.WriteLine("Nome: " + (NumeroAlunni+1) );
+        CopiaNomi[NumeroAlunni] = Console.ReadLine();
+        Console.WriteLine("Cognome: " + (NumeroAlunni + 1));
+        CopiaCognomi[NumeroAlunni] = Console.ReadLine();
+        Console.WriteLine("Età: " + (NumeroAlunni + 1));
+        do
+        {
+            sEta = Console.ReadLine();
+        } while (!int.TryParse(sEta, out iEta) || iEta > 40);
+        CopiaEta[NumeroAlunni] = iEta;
+        StampaArrayAlunni(CopiaCognomi, CopiaNomi, CopiaEta);
+    }
+}
+
+Console.WriteLine("Vuoi togliere l'ultimo alunno inserito? (si/no)");
+string RispostaUtente1 = Console.ReadLine();
+if (RispostaUtente1 == "si")
+{
+    List<int> list = new List<int>(CopiaEta);
+    list.RemoveAt(CopiaEta.Length - 1);
+    CopiaEta = list.ToArray();
+
+    List<string> list1 = new List<string>(CopiaCognomi);
+    list1.RemoveAt(CopiaCognomi.Length - 1);
+    CopiaCognomi = list1.ToArray();
+
+    List<string> list2 = new List<string>(CopiaNomi);
+    list2.RemoveAt(CopiaNomi.Length - 1);
+    CopiaNomi = list2.ToArray();
+
+    StampaArrayAlunni(CopiaCognomi, CopiaNomi, CopiaEta);
+}
+
+double risultato = sommaElementiArray(CopiaEta);
+Console.WriteLine("L'età media della classe è " + risultato);
+
+int maxValue = CopiaEta.Max();
+Console.WriteLine("L'età dell' alunno più anziano è " + maxValue);
+
+int minValue = CopiaEta.Min();
+Console.WriteLine("L'età dell' alunno più giovane è " + minValue);
+
+void InserisciAlunni (int i) 
+{ 
+ Console.WriteLine("Nome: " + (i + 1));
+ Nomi[i] = Console.ReadLine();
+ Console.WriteLine("Cognome: " + (i + 1));
+ Cognomi[i] = Console.ReadLine();
+ Console.WriteLine("Età: " + (i + 1));
+do
+{
+    sEta = Console.ReadLine();
+} while (!int.TryParse(sEta, out iEta) || iEta > 40);
+Eta[i] = iEta;
+}
 
 void StampaArrayAlunni(string[] arrayCognomi, string [] arrayNomi, int [] arrayEta )
 {
@@ -46,3 +112,16 @@ void StampaArrayAlunni(string[] arrayCognomi, string [] arrayNomi, int [] arrayE
             Console.Write(arrayCognomi[i] + " " + arrayNomi[i] + " " + arrayEta[i] + Environment.NewLine);
         }
  }
+
+double sommaElementiArray(int[] array)
+{
+    double somma = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        somma = somma + array[i];
+    }
+    somma /= array.Length;
+    return somma;
+}
+
+
